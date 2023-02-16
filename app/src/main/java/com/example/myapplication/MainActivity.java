366b72621831c641440895faf7f3ceb9a00334c3
Tobi59 <n.escaffre@hotmail.com>
2023-02-16 09:08:05 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginButton;
+
+    TextView msignupText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        msignupText = findViewById(R.id.signupText);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        msignupText.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),register.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/register.java
@@ -0,0 +1,63 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class register extends AppCompatActivity {
+
+    public static final String TAG = "TAG";
+    EditText mpasswordConfirm,mpasswordRegister, musernameRegister;
+    Button mregisterButton;
+    TextView mcreateText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        musernameRegister = findViewById(R.id.usernameRegister);
+        mpasswordRegister = findViewById(R.id.passwordRegister);
+        mpasswordConfirm = findViewById(R.id.passwordConfirm);
+        mregisterButton = findViewById(R.id.registerButton);
+        mcreateText = findViewById(R.id.createText);
+
+        mcreateText.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
+        mregisterButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                final String username = musernameRegister.getText().toString().trim();
+                String password = mpasswordRegister.getText().toString().trim();
+                String passwordConfirm = mpasswordConfirm.getText().toString().trim();
+
+                if(TextUtils.isEmpty(username)){
+                    musernameRegister.setError("Username is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mpasswordRegister.setError("Password is required");
+                    return;
+                }
+
+                if(passwordConfirm != password){
+                    mpasswordConfirm.setError("Password is different");
+                    return;
+                }
+            }
+        });
+    }
+}
