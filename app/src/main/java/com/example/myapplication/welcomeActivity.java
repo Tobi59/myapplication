eaa27f4fad19a03c7cf726ec3a4e28b6cab2c217
Lorthender <noeklop@hotmail.fr>
2023-02-22 00:07:45 +0100

    ajout d'une méthode pour récupérer des infos d'un user dans la DB

--- app/src/main/java/com/example/myapplication/welcomeActivity.java
+++ app/src/main/java/com/example/myapplication/welcomeActivity.java
@@ -6,15 +6,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class welcomeActivity extends AppCompatActivity {
     BottomNavigationView nav;
@@ -75,6 +84,23 @@ public class welcomeActivity extends AppCompatActivity {
         super.onStart();
         // vérifie que l'utilisateur n'est pas connecté, mets à jour l'UI si besoin
         FirebaseUser currentUser = mAuth.getCurrentUser();//récupère les infos de l'utilisteur actuel
+        String uid = currentUser.getUid();
+        DatabaseReference ref = FirebaseDatabase.getInstance(" https://myapplicationfirebase-7505e-default-rtdb.europe-west1.firebasedatabase.app").getReference("users").child(uid);
+        ref.get().addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                Log.e("firebase", "Error getting data", task.getException());
+            }
+            else {
+                Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                // Récupérer le résultat sous forme de Map<String, Object>
+                Map<String, Object> result = (Map<String, Object>) task.getResult().getValue();
+                // Extraire la valeur de "username"
+                String username = (String) result.get("username");
+                System.out.println(username);
+                Toast.makeText(welcomeActivity.this, "Bienvenue "+username+" !",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
         if(currentUser==null){
             startActivity(new Intent(getApplicationContext(),register.class));
         }
